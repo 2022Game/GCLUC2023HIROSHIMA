37d2baeedebacc2a20747ffde8079a2538538294
Kato-Yuki-Fukuyama <gc22015@anabuki-net.ne.jp>
2023-03-14 10:10:21 +0900

    加藤 オオカミ　スライム描画

--- GCLUC/GameProgramming/src/CGame.cpp
+++ GCLUC/GameProgramming/src/CGame.cpp
@@ -13,6 +13,7 @@ CGame::CGame()
 {
 	mpUi = new CUi();
 	//テクスチャの入力
+	CApplication::Texture()->Load(TEXTURE);
 }
 
 //デストラクタ
@@ -69,6 +70,8 @@ void CGame::Over()
 //スタート処理
 void CGame::Start()
 {
+	CTaskManager::Instance()->Update();
+	CTaskManager::Instance()-> Render();
 //	CameraSet();
 	//ゲームの描画
 //	CApplication::CharacterManager()->Render();
@@ -79,16 +82,13 @@ void CGame::Start()
 	mpUi->Render();
 	mpUi->Start();
 	if (mInput.Key(VK_RETURN))
-	{
-		mpEnemy = new CEnemy(TIPSIZE * 15, TIPSIZE * 4.5, TIPSIZE, TIPSIZE, CApplication::Texture());
+	{	//状態をプレイ中にする
+		mpWolf = new CWolf(TIPSIZE * 15, TIPSIZE * 4.5, TIPSIZE, TIPSIZE, CWolf::Texture7());
 		mpPlayer = new CPlayer(TIPSIZE * 10, TIPSIZE * 5, TIPSIZE, TIPSIZE, CApplication::Texture());
-		mpEnemy2 = new CEnemy2(TIPSIZE * 20, TIPSIZE * 7, TIPSIZE, TIPSIZE, CApplication::Texture());
+		mpSlime = new CSlime(TIPSIZE * 20, TIPSIZE * 7, TIPSIZE, TIPSIZE, CSlime::Texture6());
 		mpBackGround = new CBackGround(640.0f, 400.0f, 640.0f, 400.0f, 0, 1919, 1079, 0, CApplication::Texture3());
 		mpHeart = new CHeart(95.0f, 750.0f, 90.0f, 40.0f, 0, 210, 73, 0, CApplication::Texture100());
-		//状態をプレイ中にする
 	}
-	CTaskManager::Instance()->Update();
-	CTaskManager::Instance()->Render();
 }
 
 //更新処理
