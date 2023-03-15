4a5bd9b1de0728be28c5ee108102ee2a5b433a21
Saito-Hiromu-Fukuyama <gc22007@anabuki-net.ne.jp>
2023-03-15 11:13:17 +0900

    プレイヤーアニメーション

--- GCLUC/GameProgramming/src/CPlayer.cpp
+++ GCLUC/GameProgramming/src/CPlayer.cpp
@@ -1,44 +1,8 @@
 #include "CPlayer.h"
 #include "CApplication.h"
 
-//移動テクスチャ(右向き)
-#define MOVETEXTURE   12,  201,199, 2
-#define MOVETEXTURE2 202, 399, 186, 7
-#define MOVETEXTURE3 401, 599, 193,10
-#define MOVETEXTURE4 600, 796, 193, 8
-#define MOVETEXTURE5 798, 997, 189,17
-//移動テクスチャ(左向き)
-#define MOVETEXTURE6  201,  12, 199, 2
-#define MOVETEXTURE7  399, 202, 186, 7
-#define MOVETEXTURE8  599, 401, 193,10
-#define MOVETEXTURE9  796, 600, 193, 8
-#define MOVETEXTURE10 997, 798, 189,17
 //アイドリングテクスチャ(右向き)
-#define IDLINGTEXTURE  200, 330, 402, 218
-#define IDLINGTEXTURE2 437, 561, 402, 218
-#define IDLINGTEXTURE3 635, 763, 402, 218
-//アイドリングテクスチャ(左向き)
-#define IDLINGTEXTURE4 332, 201, 405, 213
-#define IDLINGTEXTURE5 561, 437, 402, 218
-#define IDLINGTEXTURE6 763, 635, 402, 218
-//ジャンプテクスチャ(右向き)
-#define JUMPTEXTURE   26, 185, 798, 607
-#define JUMPTEXTURE2 218, 382, 800, 603
-//ジャンプテクスチャ(左向き)
-#define JUMPTEXTURE3 185,  26, 798, 607
-#define JUMPTEXTURE4 382, 218, 800, 603
-//攻撃テクスチャ(右向き)
-#define ATTACKTEXTURE  832, 961, 402, 218
-#define ATTACKTECTURE2  30, 181, 606, 418 
-//攻撃テクスチャ(左向き)
-#define ATTACKTEXTURE  916, 832, 402, 218
-#define ATTACKTECTURE2  181, 30, 606, 418 
-//ダメージテクスチャ
-#define DAMAGETEXTURE 217, 381, 602, 421
-//死亡テクスチャ
-#define DEATHTEXTURE  635, 775, 599, 421
-#define DEATHTEXTURE2 798, 995, 593, 492
-
+#define DEFAULT  0, 200, 400, 200
 
 #define JUMPV0 30               	//ジャンプの初速
 #define GRAVITY 2.0f	            //重力加速度
@@ -54,13 +18,16 @@ int CPlayer::sStamina = 0;
 CPlayer::CPlayer(float x, float y, float w, float h, CTexture* pt)
 	: CCharacter((int)ETaskPriority::ECharacter)
 	, mInvincible(0)
-	,mIdlingcount(0)
+	, mIdlingcount(0)
+	, mJumpcount(0)
+	, mMovecount(0)
+	, mAttackcount(0)
 	, mJumpY(0.0f)
 {
-	Set(x, y, w, h*1.5);
-	Texture(pt, IDLINGTEXTURE);
+	Set(x, y, w, h * 1.5);
+	Texture(pt, DEFAULT);
 	spinstance = this;
-	mState = EState::EMOVE;
+	mState = EState::EIDLING;
 	sHp = PLAYERHP;
 	sStamina = STAMINA;
 }
@@ -91,6 +58,8 @@ int CPlayer::Stamina()
 //更新処理
 void CPlayer::Update()
 {
+	float left;
+	float right;
 	switch (State())
 	{
 	case EState::EMOVE:
@@ -120,25 +89,35 @@ void CPlayer::Update()
 			Y(Y() - mVy);
 			mIdlingcount = 0;
 		}
+
+		mMovecount %= 48;
+		left = (mMovecount / 8) * 200;
+		right = left + 200;
+		mMovecount++;
+		if (mVx >= 0)
+		{
+			Texture(Texture(), left, right, 200, 0);
+		}
+		if (mVx < 0)
+		{
+			Texture(Texture(), right, left, 200, 0);
+		}
 		//攻撃
 		if (mInput.Key('K'))
 		{
+			mAttackcount = 0;
 			mState = EState::EATTACK;
 		}
 		//キー入力がされていないかつカウントが0以上の時
 		if (mInput.Key('W') != true ||
-			mInput.Key('A') != true || 
-			mInput.Key('S') != true || 
+			mInput.Key('A') != true ||
+			mInput.Key('S') != true ||
 			mInput.Key('D') != true ||
 			mInput.Key('J') != true ||
 			mInput.Key('K') != true)
 		{
-			if (mIdlingcount <= 0)
-			{
-				mIdlingcount = 20;
-			}
 			mIdlingcount--;
-			if (mIdlingcount <= 10)
+			if (mIdlingcount <= -10)
 			{
 				mState = EState::EIDLING;
 			}
@@ -149,7 +128,7 @@ void CPlayer::Update()
 			{
 				mJumpY = Y();
 				mVy = JUMPV0;
-				mInvincible = 25;
+				mInvincible = 20;
 				mState = EState::EJUMP;
 			}
 		}
@@ -160,14 +139,33 @@ void CPlayer::Update()
 		//Y軸速度に重力を減算する
 		mVy -= GRAVITY;
 		Y(Y() + mVy);
+
+		mJumpcount %= 30;
+		left = (mJumpcount / 10) * 200;
+		right = left + 200;
+		mJumpcount++;
+		if (mVx >= 0)
+		{
+			Texture(Texture(), left, right, 800, 600);
+		}
+		else
+		{
+			Texture(Texture(), right, left, 800, 600);
+		}
+
 		if (mJumpY >= Y())
 		{
-			//無敵カウンタが0になったら状態をMOVEにする
+			//無敵カウンタが0になったら状態をIDLINGにする
 			if (mInvincible <= 0)
 			{
-				mState = EState::EMOVE;
+				mState = EState::EIDLING;
+			}
+			else if (mJumpcount < 29)
+			{
+				mState = EState::EIDLING;
 			}
 		}
+		//X軸方向移動
 		if (mInput.Key('A'))
 		{
 			mVx = -VELOCITY;
@@ -179,46 +177,51 @@ void CPlayer::Update()
 			X(X() + mVx);
 		}
 		break;
+
 	case EState::EIDLING:
-		if (mInput.Key('W') || mInput.Key('A') || 
+		if (mInput.Key('W') || mInput.Key('A') ||
 			mInput.Key('S') || mInput.Key('D') ||
 			mInput.Key('J') || mInput.Key('K'))
 		{
 			mState = EState::EMOVE;
+			mIdlingcount = 0;
 		}
-		if (mIdlingcount <= 0)
+		mIdlingcount %= 72;
+		left = (mIdlingcount / 18) * 200;
+		right = left + 200;
+		if (mVx >= 0)
 		{
-			mIdlingcount = 72;
+			Texture(Texture(), left, right, 400, 200);
 		}
 		else
 		{
-			mIdlingcount--; 
+			Texture(Texture(), right, left, 400, 200);
 		}
-		if (mIdlingcount == 72)
-		{
-			if (mVx >= 0) { Texture(Texture(), IDLINGTEXTURE); }
-			if (mVx < 0) { Texture(Texture(), IDLINGTEXTURE4); }
-		}
-		if (mIdlingcount == 54)
-		{
-			if (mVx >= 0) { Texture(Texture(), IDLINGTEXTURE2); }
-			if (mVx < 0) { Texture(Texture(), IDLINGTEXTURE5); }
-		}
-		if (mIdlingcount == 36)
-		{
-			if (mVx >= 0) { Texture(Texture(), IDLINGTEXTURE3); }
-			if (mVx < 0) { Texture(Texture(), IDLINGTEXTURE6); }
-		}
-		if (mIdlingcount == 18)
-		{
-			if (mVx >= 0) { Texture(Texture(), IDLINGTEXTURE2); }
-			if (mVx < 0) { Texture(Texture(), IDLINGTEXTURE5); }
-		}
+		mIdlingcount++;
 		break;
+
 	case EState::EATTACK:
+		mAttackcount++;
+		mAttackcount %= 24;
+		left = (mAttackcount / 12) * 200;
+		right = left + 200;
+		if (mVx >= 0)
+		{
+			Texture(Texture(), left, right, 600, 400);
+		}
+		else
+		{
+			Texture(Texture(), right, left, 600, 400);
+		}
+		if (mAttackcount >= 23)
+		{
+			mState = EState::EIDLING;
+		}
 		break;
+
 	case EState::EDAMAGE:
 		break;
+
 	case EState::EDEATH:
 		break;
 
@@ -231,4 +234,3 @@ void CPlayer::Update()
 	}
 	CCharacter::Update();
 }
-
